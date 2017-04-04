4d121df619cd87f488331837b33c4d0bca380997
captraining2017 <captrainingfeb2017@gmail.com>
2017-04-04 20:19:50 +0530

    Added Fraud detection code

---
+++ Fraud_Detection/src/com/capgemini/hackathon/automation/main/fraudDetection.java
@@ -0,0 +1,16 @@
+package com.capgemini.hackathon.automation.main;
+
+import com.capgemini.hackathon.automation.service.ObjectBuilder;
+
+public class fraudDetection {
+
+	public static void main(String[] args) {
+		
+		ObjectBuilder obj = new ObjectBuilder();
+		obj.buildCustomerTransactionModel();        
+        
+    }
+
+	
+    
+}
