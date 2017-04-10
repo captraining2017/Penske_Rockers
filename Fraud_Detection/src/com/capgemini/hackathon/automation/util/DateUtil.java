773a9ae59700c7ae022fb83438d66a8b8a82aac8
captraining2017 <captrainingfeb2017@gmail.com>
2017-04-10 10:22:16 +0530

    Added 2 more new methods

--- Fraud_Detection/src/com/capgemini/hackathon/automation/util/DateUtil.java
+++ Fraud_Detection/src/com/capgemini/hackathon/automation/util/DateUtil.java
@@ -6,10 +6,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class DateUtil {
-	
+	private static final String [] months={"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
 	public static List<Date> getMonthDateList(int month, Date date) {
 		List<Date> eachMonthDateList = new ArrayList<>();
 		Calendar cal = Calendar.getInstance();
@@ -38,6 +39,40 @@ public class DateUtil {
 	public static Date getFormatedDate(String dateFormat, String txnDate) throws ParseException {
 		DateFormat formatter = new SimpleDateFormat(dateFormat);
 		return formatter.parse(txnDate);
+	}
+	
+	public static String getMonthYear(Date date) {
+		String monthYear=null;
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		monthYear=months[month] + "_"+ year;
+		return monthYear;
+	}
+	
+	public static boolean isDateCurrentMonth(Date date, Integer currentMonthIndex, Integer currentYear) {
+		boolean flag=false;
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		flag = (currentYear == year) && (currentMonthIndex == month); 
+		return flag;
+	}
+	
+	public static int getMonth(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int month = calendar.get(Calendar.MONTH);
+		return month;
+	}
+	
+	public static int getYear(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		return year;
 	}
 		
 }
